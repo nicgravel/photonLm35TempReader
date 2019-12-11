2d3c26b29e161c311d377edc571b09408190e5c3
Nicolas Gravel <ngravel@myca.com>
2019-12-11 08:38:50 -0500

    round numbers

--- src/main.js
+++ src/main.js
@@ -6,26 +6,37 @@ const mysql = require('mysql');
 const util = require('util');
 const db = mysql.createPool(config.mysql.connection);
 db.query = util.promisify(db.query).bind(db);
-const interval = config.interval * 1000;
+let interval = config.interval * 1000;
+
+
 main();
 
 
 async function main() {
+    const myArgs = process.argv.slice(2);
+    if(myArgs.length){
+        interval = myArgs[0] * 1000;
+    }
     timedCheckTemp();
 }
 
 async function timedCheckTemp() {
     const curTemp = await getTemp();
-    saveTempInDb(curTemp);
-    setTimeout( timedCheckTemp, interval)
+    (curTemp) ? saveTempInDb(curTemp) : console.log("No value received");
+    setTimeout(timedCheckTemp, interval)
 }
 
 async function getTemp() {
-    const curTemp = await Axios.get(config.url, {headers: {'Authorization': "Bearer " + config.token}})
-    const date = new Date();
-    const result = parseFloat(curTemp.data.result).toFixed(2);
-    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} => ${result}`);
-    return result;
+    try {
+        const curTemp = await Axios.get(config.url, { headers: { 'Authorization': "Bearer " + config.token } })
+        const date = new Date();
+        const result = roundValue(curTemp.data.result);
+        console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} => ${result}`);
+        return result;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
 }
 
 async function saveTempInDb(data) {
@@ -36,4 +47,13 @@ async function saveTempInDb(data) {
     } catch (err) {
         console.dir(err);
     }
+}
+
+function roundValue(temp, roundToPoint5 = false) {
+    if (roundToPoint5) {
+        const newTemp = parseFloat(temp).toFixed(2);
+        const y = +newTemp + (0.5 / 2);
+        return y - (y % (+0.5));
+    }
+    return parseFloat(temp).toFixed(1);
 }
